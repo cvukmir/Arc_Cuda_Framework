9b7ee9d00e315c32ea310edf5f509f6aaa25188b
Chris <vukmirchristopher@gmail.com>
2023-11-13 21:36:37 -0600

    Exponential Ranks
    Adds in a neat and fast way to send data to multiple ranks fast without using broadcast method.

--- ArcCuda/ArcMpi/ArcMpi.cpp
+++ ArcCuda/ArcMpi/ArcMpi.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <chrono>
 
 // Part 1:
 // Rank 0 sends an int 0 to rank 1 and prints statement
@@ -30,10 +31,19 @@ int main(int argc, char* argv[])
 {
 	MPI_Init(&argc, &argv);
 	
+	//ArcMpi::assignment1();
+
+	ArcMpi::assignment2();
+
+	MPI_Finalize();
+
+	return 0;
+}
+
+void ArcMpi::assignment1()
+{
 	int rankCount = 0;
 	int myRank    = 0;
-
-	int value = 0;
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 	MPI_Comm_size(MPI_COMM_WORLD, &rankCount);
@@ -45,26 +55,108 @@ int main(int argc, char* argv[])
 		std::cout << std::endl << "---Part 1---" << std::endl;
 	}
 
-	//MPI_Barrier(MPI_COMM_WORLD);
-
 	ArcMpi::part1(myRank, rankCount);
-
-	//MPI_Barrier(MPI_COMM_WORLD);
 
 	if (myRank == 0)
 	{
 		std::cout << std::endl << "---Part 2---" << std::endl;
 	}
 
-	//MPI_Barrier(MPI_COMM_WORLD);
-
 	ArcMpi::part2(myRank, rankCount);
+}
 
-	//MPI_Barrier(MPI_COMM_WORLD);
+void ArcMpi::assignment2()
+{
+	int rankCount = 0;
+	int myRank    = 0;
+	int value     = 3;
 
-	MPI_Finalize();
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &rankCount);
 
-	return 0;
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	if (myRank == 0)
+	{
+		broadcastSend(myRank, rankCount, value);
+	}
+	else
+	{
+		broadcastRecv(myRank, rankCount, value);
+	}
+
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if (myRank == 0)
+	{
+		std::cout << "Sent data to ranks exponentially in " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " microseconds." << '\n';
+	}
+	
+	MPI_Barrier(MPI_COMM_WORLD);
+	
+	begin = std::chrono::steady_clock::now();
+	
+	if (myRank == 0)
+	{
+		sequentialSend(myRank, rankCount, value);
+	}
+	else
+	{
+		sequentialRecv(myRank, rankCount, value);
+	}
+	
+	end = std::chrono::steady_clock::now();
+	
+	if (myRank == 0)
+	{
+		std::cout << "Sent data to ranks sequentially in " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " microseconds." << '\n';
+	}
+	//std::cout << "Rank " << myRank << " checking in." << std::endl;
+}
+
+void ArcMpi::broadcastSend(const int myRank, const int rankCount, const int value)
+{
+	if (rankCount < 2)
+	{
+		return;
+	}
+
+	// This rank is always even.
+	for (int i = 1; i < rankCount; i = (i << 1) + 1)
+	{
+		MPI_Send(&value, 1, MPI_INT, i, 99, MPI_COMM_WORLD);
+		//std::cout << "Rank " << myRank << " sent value " << value << " to rank " << i << std::endl;
+	}
+}
+
+void ArcMpi::broadcastRecv(const int myRank, const int rankCount, int& value)
+{
+	MPI_Status status;
+
+	//std::cout << "Rank " << myRank << " wating..." << std::endl;
+	MPI_Recv(&value, 1, MPI_INT, MPI_ANY_SOURCE, 99, MPI_COMM_WORLD, &status);
+	//std::cout << "Rank " << myRank << " recieved value " << value << std::endl;
+
+	if (myRank & 0x00000001)
+	{
+		// This rank is odd.
+		for (int i = myRank << 1; i < rankCount; i = (i << 1))
+		{
+			MPI_Send(&value, 1, MPI_INT, i, 99, MPI_COMM_WORLD);
+			//std::cout << "Rank " << myRank << " sent value " << value << " to rank " << i << std::endl;
+		}
+	}
+	else
+	{
+		// This rank is even.
+		for (int i = (myRank << 1) + 1; i < rankCount; i = (i << 1) + 1)
+		{
+			MPI_Send(&value, 1, MPI_INT, i, 99, MPI_COMM_WORLD);
+			//std::cout << "Rank " << myRank << " sent value " << value << " to rank " << i << std::endl;
+		}
+	}
 }
 
 void ArcMpi::part1(const int myRank, const int rankCount)
@@ -171,4 +263,19 @@ void ArcMpi::part2(const int myRank, const int rankCount)
 
 		MPI_Send(&value, 1, MPI_INT, myRank - 1, 99, MPI_COMM_WORLD);
 	}
+}
+
+void ArcMpi::sequentialSend(const int myRank, const int rankCount, const int value)
+{
+	for (int i = 1; i < rankCount; ++i)
+	{
+		MPI_Send(&value, 1, MPI_INT, i, 99, MPI_COMM_WORLD);
+		//std::cout << "Rank " << myRank << " sent value " << value << " to rank " << i << std::endl;
+	}
+}
+
+void ArcMpi::sequentialRecv(const int myRank, const int rankCount, int& value)
+{
+	MPI_Status status;
+	MPI_Recv(&value, 1, MPI_INT, MPI_ANY_SOURCE, 99, MPI_COMM_WORLD, &status);
 }
--- ArcCuda/ArcMpi/ArcMpi.h
+++ ArcCuda/ArcMpi/ArcMpi.h
@@ -4,8 +4,17 @@
 class ArcMpi
 {
 public:
+	static void assignment1();
+	static void assignment2();
+
+	static void broadcastSend(const int myRank, const int rankCount, const int value);
+	static void broadcastRecv(const int myRank, const int rankCount, int& value);
+
 	static void part1(const int myRank, const int rankCount);
 	static void part2(const int myRank, const int rankCount);
+	
+	static void sequentialSend(const int myRank, const int rankCount, const int value);
+	static void sequentialRecv(const int myRank, const int rankCount, int& value);
 };
 
 #endif // !ARC_MPI_H
